06d2902462d3dcad45027ab2c89c5271bd4ea114
Glitch (hello-express) <none>
2019-05-04 19:55:14 +0000

    🏙📇 Checkpoint ./watch.json:1496189/8 ./package.json:1496189/1432 ./config.json:1496189/1 ./index.js:1496189/345

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ setInterval(() => {
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+client.on("ready", () => {
+console.log(`${client.user.tag} gotowy do pracy na ${client.guilds.size} serwerach przy ${client.users.size} użytkownikach!`);
+})
 client.on("message", (message) => {
 const prefix = config.prefix;
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
   if (command === "pw" && message.author.id == "403666583177920532" || message.author.id == "558380095870468169") {
      let pwmessage = args.slice(0).join(" ");
-    message.guild.members.forEach(member => {
+    message.guild.members.forEach((member, members) => {
       if (member.id != client.user.id && !member.user.bot) member.send(pwmessage);
+      message.channel.send(`Wysłano do ${members.size}`)
     });
   }
 });
