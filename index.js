07fcee38f019c3321371d98c3b0021cf7a97082c
Glitch (hello-express) <none>
2019-05-04 19:33:16 +0000

    ⚽️🛢 Checkpoint ./index.js:1496189/182 ./config.json:1496189/86

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 client.on("message", (message) => {
-const prefix = "+";
+const prefix = config.prefix;
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
   if (command === "pw") {
      let pwmessage = args.slice(0).join(" ");
-    
+    message.guild.members.forEach(member => {
+      if (member.id != client.user.id && !member.user.bot) member.send(pwmessage);
+    });
   }
 });
+client.login(config.token)
