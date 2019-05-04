586cfd927bb8eb92fa5f6b63cdae67b5e91f62fa
Glitch (hello-express) <none>
2019-05-04 20:06:13 +0000

    🛥🏗 Checkpoint ./index.js:1496189/903 ./watch.json:1496189/8

--- index.js
+++ index.js
@@ -13,18 +13,22 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 client.on("ready", () => {
-console.log(`${client.user.tag} gotowy do pracy na ${client.guilds.size} serwerach przy ${client.users.size} użytkownikach!`);
+console.log(`${client.user.tag} gotowy do pracy na ${client.guilds.size} serwerach przy ${client.users.filter(user => !user.bot).size} użytkownikach!`);
 })
 client.on("message", (message) => {
 const prefix = config.prefix;
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
-  if (command === "pw" && message.author.id == "403666583177920532" || message.author.id == "558380095870468169") {
+  if (command === "pw") {
+ if (message.author.id == "403666583177920532" || message.author.id == "558380095870468169") {
      let pwmessage = args.slice(0).join(" ");
-    message.guild.members.forEach((member, members) => {
+    message.guild.members.forEach(member => {
       if (member.id != client.user.id && !member.user.bot) member.send(pwmessage);
-      message.channel.send(`Wysłano do ${members.size}`)
     });
+  message.channel.send(`Wysłano podaną wiadomość do ${client.users.filter(user => !user.bot).size} użytkowników`)
+  } else {
+message.channel.send("Nie masz permisji do użycia tej komendy!")
   }
+}
 });
 client.login(config.token)
