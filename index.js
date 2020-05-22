a9f2fe7865af662b8f5b796fa96b2850b243d631
Glitch (hello-express) <none>
2020-05-22 20:46:44 +0000

    👷🎖 Checkpoint ./config.json:4349905/120 ./watch.json:4349905/4 ./index.js:4349905/241

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ client.on("message", (message) => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     if (command === "pw") { //Za to łatwo bana wyłapać :>
-        if (message.author.id == "688161946377257002") {
-            message.guild.members.forEach(member => {
+        if (message.author.id == "380427062390947852"  
                 if (member.id != client.user.id && !member.user.bot) member.send(args);
             });
         message.channel.send(`Wysłano podaną wiadomość do ${client.users.filter(user => !user.bot).size} użytkowników`) //nie polecam używać na kanale głównym bo przyps, w dodatku kto by chciał coś wysyłać każdemu użytkownikowi na serwerze
@@ -39,20 +38,20 @@ client.on("message", (message) => {
     //unikałbym takiej składni w przyszłości, troche tu niepoukładanie
     if (message.mentions.users.get(client.user.id) || command === "help" || command === "pomoc") {
         message.channel.send('Cześc! Moje Komendy to:\n```' + config.prefix + 'pw, ' + config.prefix + 'powiedz, ' + config.prefix + 'status```\n**Permisje do używania komend ma tylko: ' 
-                             + client.users.find(user => user.id == 688161946377257002).tag + '!**')
+                             + client.users.find(user => user.id == 380427062390947852).tag + '!**')
     } else if (message.content.startsWith('Cześć') || message.content.startsWith('Cześć!') || message.content.startsWith('Czesc')) {
         message.channel.send("Cześć!");
     }  else if (message.content.startsWith('Hej') || message.content.startsWith('Hej!')) { message.channel.send('Hej!'); }
     
     if (command === "powiedz") {
-        if (!message.author.id == "688161946377257002") return message.channel.send("Nie masz permisji do użycia tej komendy!"); //poprawiłem trochę kod by było bardziej przejrzyście
+        if (!message.author.id == "380427062390947852") return message.channel.send("Nie masz permisji do użycia tej komendy!"); //poprawiłem trochę kod by było bardziej przejrzyście
         var wiadomosc = args.slice(0).join(" ");
         if (!wiadomosc) return message.reply("Nie napisano żadnej wiadomości");
         message.channel.send(wiadomosc)
     }
     
     if (command === "status") {
-        if (!message.author.id == "688161946377257002") return message.channel.send("Nie masz permisji do użycia tej komendy!"); 
+        if (!message.author.id == "380427062390947852") return message.channel.send("Nie masz permisji do użycia tej komendy!"); 
         var wiadomosc = args.slice(0).join(" ");
         if (!wiadomosc) return message.channel.send("RONACORD");
         client.user.setActivity(wiadomosc);
