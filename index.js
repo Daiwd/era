ea2de2c2ca20c4ab30188dd959fb633f6ce61a03
Glitch (louse) <none>
2020-08-23 20:19:46 +0000

    Rewound to commit b61ccedb90876528c929799a4cca24f9bcc960c9 + 0 edits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //glitch.com
-const http = require("http");
-const express = require("express");
+const http = require('http');
+const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
@@ -11,156 +11,54 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
+
 //bot
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
-  console.log(
-    `${client.user.tag} gotowy do pracy na ${
-      client.guilds.size
-    } serwerach przy ${
-      client.users.filter(user => !user.bot).size
-    } użytkownikach!`
-  );
-});
+    console.log(`${client.user.tag} gotowy do pracy na ${client.guilds.size} serwerach przy ${client.users.filter(user => !user.bot).size} użytkownikach!`);
+})
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  const prefix = config.prefix;
-  const args = message.content
-    .slice(prefix.length)
-    .trim()
-    .split(/ +/g);
-  const command = args.shift().toLowerCase();
-  /*if (command === "pw") {
-    //Za to łatwo bana wyłapać :>
-    if (message.author.id == "688161946377257002") {
-      var msg = args.join(" ").toString();
-      message.guild.members.forEach(member => {
-        if (member.id != client.user.id && !member.user.bot) {
-          const embed = new Discord.RichEmbed()
-            .setColor("#FFFF00")
-            .setDescription(msg);
-          member.send(embed);
+client.on("message", (message) => {
+    if (message.author.bot) return;
+    const prefix = config.prefix;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (command === "pw") { //Za to łatwo bana wyłapać :>
+        if (message.author.id == "380427062390947852")   {
+          var members = message.guild.members;
+          members.tap(member => {
+            if (member.id != client.user.id && !member.user.bot) member.send(args);
+          message.channel.send(`Wysłano podaną wiadomość do ${members.size} użytkowników`);  
+          })
+        } else { 
+            message.channel.send("Nie masz permisji do użycia tej komendy!")
         }
-      });
-      message.channel.send(
-        `Wysłano podaną wiadomość do ${
-          client.users.filter(user => !user.bot).size
-        } użytkowników`
-      ); //nie polecam używać na kanale głównym bo przyps, w dodatku kto by chciał coś wysyłać każdemu użytkownikowi na serwerze
-    } else {
-      message.channel.send("Nie masz permisji do użycia tej komendy!");
     }
-  }*/
 
-  //unikałbym takiej składni w przyszłości, troche tu niepoukładanie
-  /*if (
-    message.mentions.users.get(client.user.id) ||
-    command === "help" ||
-    command === "pomoc"
-  ) {
-    message.channel.send(
-      "**LISTA KOMEND ADMINISTRACJI**\n```" +
-        config.prefix +
-        "PW - Wysyła do kazdego na serwerze wiadomość" +
-        config.prefix +
-        "powiedz - Wysyła wiadomość na danym kanale " +
-        config.prefix +
-        "status```\n**ADMINISTRACJA MOŻE TYLKO UŻYWAĆ!" +
-        client.users.find(user => user.id == "688161946377257002").tag +
-        "!**"
-    );
-  } else if (
-    message.content.startsWith("Cześć") ||
-    message.content.startsWith("Cześć!") ||
-    message.content.startsWith("Czesc")
-  ) {
-    message.channel.send("Cześć!");
-  } else if (
-    message.content.startsWith("Hej") ||
-    message.content.startsWith("Hej!")
-  ) {
-    message.channel.send("Hej!");
-  }*/
-  
-  if (command === "status") {
-    if (!message.content.startsWith(prefix)) return;
-    if (!message.author.id == "688161946377257002")
-      return message.channel.send("Nie masz permisji do użycia tej komendy!");
-    message.delete();
-    var wiadomosc = args.slice(0).join(" ");
-    if (!wiadomosc) return message.channel.send("RONACORD");
-    client.user.setActivity(wiadomosc);
-    message.channel.send("Ustawiono");
-  }
-
-  if (command == "ping") {
-    if (!message.content.startsWith(prefix)) return;
-    var rola = message.member.roles.get("715511386809172038");
-    var wiadomosc = args
-      .slice(1)
-      .join(" ")
-      .toString();
-    if (!wiadomosc) return;
-    if (!rola) return message.reply("Nie posiadasz roli " + rola.toString());
-    message.delete(); //usuwanie wiadomości użytkownika
-    var pinged_role = message.guild.roles.get(args[0]);
-    if (!pinged_role) return;
-    message.channel.send(`${pinged_role.toString()} - ${wiadomosc}`); //wysyła ping
-  }
-
-  if (command == "tak") {
-    if (!message.content.startsWith(prefix)) return;
-    message.member.addRole("715506096537600051");
-  }
-
-  if (command == "nie") {
-    if (!message.content.startsWith(prefix)) return;
-    message.member.ban("Dostał wywalony ponieważ nie ma ukończone 13 lat ;(");
-  }
-
-client.on("message", message => {
-  if (message.author.bot) return;
-  const prefix = config.prefix;
-  const args = message.content
-    .slice(prefix.length)
-    .trim()
-    .split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-let muteRole = message.guild.roles.get("715512265620848681");
-    let powod = args
-      .slice(2)
-      .join(" ")
-      .toString();
-    let member = message.mentions.members.first();
-    let cooldown = parseInt(args[1]);
-    console.log(
-      muteRole.name + " " + powod + " " + member.user.tag + " " + cooldown
-    );
-    if (member.roles.get(muteRole.id)) return;
-    member.addRole(muteRole.id).then(muted => {
-      console.log("muted");
-      const embed = new Discord.RichEmbed()
-        .setDescription(
-          `Użytkownik ${muted.user.tag} został zmutowany przez ${message.author.tag} za \`${powod}\` na ${cooldown} sekund`
-        )
-        .setColor("#fffff0");
-      message.channel.send(embed);
-      member.send(
-        embed
-          .setDescription(
-            `Zostałeś zmutowany przez ${message.author.tag} za \`${powod}\` na ${cooldown} sekund`
-          )
-          .setFooter(member.user.avatarURL)
-      );
-      setTimeout(function() {
-        muted.removeRole(muteRole);
-      }, cooldown * 1000);
-    });
-  }
+    //unikałbym takiej składni w przyszłości, troche tu niepoukładanie
+    if (message.mentions.users.get(client.user.id) || command === "help" || command === "pomoc") {
+        message.channel.send('Cześc! Moje Komendy to:\n```' + config.prefix + 'pw, ' + config.prefix + 'powiedz, ' + config.prefix + 'status```\n**Permisje do używania komend ma tylko: ' 
+                             + client.users.find(user => user.id == 380427062390947852).tag + '!**')
+    } else if (message.content.startsWith('Cześć') || message.content.startsWith('Cześć!') || message.content.startsWith('Czesc')) {
+        message.channel.send("Cześć!");
+    }  else if (message.content.startsWith('Hej') || message.content.startsWith('Hej!')) { message.channel.send('Hej!'); }
+    
+    if (command === "powiedz") {
+        if (!message.author.id == "380427062390947852") return message.channel.send("Nie masz permisji do użycia tej komendy!"); //poprawiłem trochę kod by było bardziej przejrzyście
+        var wiadomosc = args.slice(0).join(" ");
+        if (!wiadomosc) return message.reply("Nie napisano żadnej wiadomości");
+        message.channel.send(wiadomosc)
+    }
+    
+    if (command === "status") {
+        if (!message.author.id == "380427062390947852") return message.channel.send("Nie masz permisji do użycia tej komendy!"); 
+        var wiadomosc = args.slice(0).join(" ");
+        if (!wiadomosc) return message.channel.send("RONACORD");
+        client.user.setActivity(wiadomosc);
+        message.channel.send("Ustawiono podany status!")
+    }
 });
-client.login(config.token);
+client.login(config.token)
