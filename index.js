459a6afb004d7a00d2a7b603fc54dcecbcd8cc1e
Glitch (louse) <none>
2020-08-28 09:56:59 +0000

    Rewound to commit 1257a27aa98a7d7422e45d2dce6d7529caf8c15f + 0 edits

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on("message", message => {
     .trim()
     .split(/ +/g);
   const command = args.shift().toLowerCase();
-  /*if (command === "pw") {
+  if (command === "pw") {
     //Za to łatwo bana wyłapać :>
     if (message.author.id == "688161946377257002") {
       var msg = args.join(" ").toString();
@@ -42,7 +42,7 @@ client.on("message", message => {
         if (member.id != client.user.id && !member.user.bot) {
           const embed = new Discord.RichEmbed()
             .setColor("#FFFF00")
-            .setDescription(msg);
+            .setDescription(msg)
           member.send(embed);
         }
       });
@@ -54,10 +54,10 @@ client.on("message", message => {
     } else {
       message.channel.send("Nie masz permisji do użycia tej komendy!");
     }
-  }*/
+  }
 
   //unikałbym takiej składni w przyszłości, troche tu niepoukładanie
-  /*if (
+  if (
     message.mentions.users.get(client.user.id) ||
     command === "help" ||
     command === "pomoc"
@@ -84,25 +84,22 @@ client.on("message", message => {
     message.content.startsWith("Hej!")
   ) {
     message.channel.send("Hej!");
-  }*/
+  }
 
   if (command === "powiedz") {
-    if (!message.content.startsWith(prefix)) return;
     if (!message.author.id == "688161946377257002")
-      return message.channel.send("Nie masz permisji do użycia tej komendy!");
+      return message.channel.send("Nie masz permisji do użycia tej komendy!"); //poprawiłem trochę kod by było bardziej przejrzyście
     var wiadomosc = args.slice(0).join(" ");
     if (!wiadomosc) return message.reply("Nie napisano żadnej wiadomości");
     const embed = new Discord.RichEmbed()
-      .setColor("#ffff45")
-      .setDescription(wiadomosc);
+    .setColor("#ffff45")
+    .setDescription(wiadomosc)
     message.channel.send(embed);
   }
 
   if (command === "status") {
-    if (!message.content.startsWith(prefix)) return;
     if (!message.author.id == "688161946377257002")
       return message.channel.send("Nie masz permisji do użycia tej komendy!");
-    message.delete();
     var wiadomosc = args.slice(0).join(" ");
     if (!wiadomosc) return message.channel.send("RONACORD");
     client.user.setActivity(wiadomosc);
@@ -110,83 +107,60 @@ client.on("message", message => {
   }
 
   if (command == "ping") {
-    if (!message.content.startsWith(prefix)) return;
-    var rola = message.member.roles.get("715511386809172038");
-    var wiadomosc = args
-      .slice(1)
-      .join(" ")
-      .toString();
-    if (!wiadomosc) return;
-    if (!rola) return message.reply("Nie posiadasz roli " + rola.toString());
     message.delete(); //usuwanie wiadomości użytkownika
-    var pinged_role = message.guild.roles.get(args[0]);
-    if (!pinged_role) return;
-    message.channel.send(`${pinged_role.toString()} - ${wiadomosc}`); //wysyła ping
+    var role = message.guild.roles.get("715511386809172038"); //id organizatora eventów
+    var pinged_role = message.guild.roles.get(" "); //rola którą się pinguje
+    if (!message.member.roles.get(role)) return; //jeśli ktoś nie ma roli zdefiniowanej wyżej to cofa
+    message.channel.send(`${pinged_role.toString()}`); //wysyła ping
   }
 
   if (command == "tak") {
-    if (!message.content.startsWith(prefix)) return;
-    message.member.addRole("715506096537600051");
+    message.members.addRole("715506096537600051");
   }
-
+  
   if (command == "nie") {
-    if (!message.content.startsWith(prefix)) return;
     message.member.ban("Dostał wywalony ponieważ nie ma ukończone 13 lat ;(");
   }
-
-  if (command == "rola-dodaj") {
-    if (!message.content.startsWith(prefix)) return;
-    var roleMention = args
-      .slice(1)
-      .join(" ")
-      .toString();
-    var role = message.guild.roles.find(r => r.name == roleMention);
-    var memberMention = message.mentions.members.first();
-    if (!roleMention && !memberMention) return;
-    if (
-      !message.member.roles.get("715942031779692544") &&
-      !message.member.roles.get("715942039715446876")
-    ) {
-      return message.reply("Nie posiadasz permisji do użycia tej komendy");
+  
+  if (message.channel.id == "715554804134314034") {
+    //id kanalu do roli
+    if (message.guild.roles.find(role => role.name == message.content)) {
+      if (message.author.bot) return;
+      if (message.mentions.roles) return;
+      var roleToGive = message.guild.roles.find(
+        role => role.name == message.content
+      );
+      var bannedRoles = [
+        "715429731629006869",
+        "715505723555184692",
+        "715429827532029952",
+        "715504036433887315",
+        "715505013702525010",
+        "715505013702525010",
+        "715504961227587594",
+        "715496397880688650",
+        "715506095271051306",
+        "715506096537600051",
+        "715506019794681920",
+        "715505905898225725",
+        "715511561111994378",
+        "715511561111994378",
+        "715512267650760765",
+        "715546266033193031"
+      ];
+      if (bannedRoles.includes(roleToGive)) return;
+      message.member.addRole(roleToGive);
     }
-    /*var bannedRoles = [
-      "715429731629006869",
-      "715505723555184692",
-      "715429827532029952",
-      "715504036433887315",
-      "715505013702525010",
-      "715505013702525010",
-      "715504961227587594",
-      "715496397880688650",
-      "715506095271051306",
-      "715506096537600051",
-      "715506019794681920",
-      "715505905898225725",
-      "715511561111994378",
-      "715511561111994378",
-      "715512267650760765",
-      "715546266033193031"
-    ];
-    if (bannedRoles.includes(mention.id)) return;*/
-    memberMention.addRole(role).then(member => {
-      const embed = new Discord.RichEmbed()
-        .setDescription(
-          `Użytkownik ${memberMention.user.tag} otrzymał rolę ${role.name}`
-        )
-        .setColor("#6bff00");
-      message.channel.send(embed);
-    });
   }
 
   if (command == "propozycja") {
-    if (!message.content.startsWith(prefix)) return;
     var suggestion = args.join(" ").toString();
     const embed = new Discord.RichEmbed()
-      .setColor("#6bff00")
+      .setColor("#00ff00")
       .setAuthor(message.author.tag)
       .setDescription(suggestion);
     message.guild.channels
-      .get("716573614979416115")
+      .get("715520608502546463")
       .send(embed)
       .then(sentMessage => {
         sentMessage.react("👍");
@@ -195,110 +169,18 @@ client.on("message", message => {
   }
 
   if (command == "skarga") {
-    if (!message.content.startsWith(prefix)) return;
     var skarga = args.join(" ").toString();
     const embed = new Discord.RichEmbed()
-      .setColor("#db8a8a")
+      .setColor("#ff4d4d")
       .setAuthor(message.author.tag)
       .setDescription(skarga);
     message.guild.channels
-      .get("716573614979416115")
+      .get("715520630849667114")
       .send(embed)
       .then(sentMessage => {
         sentMessage.react("👍");
         sentMessage.react("👎");
       });
-  }
-
-  if (command == "kick") {
-    if (!message.content.startsWith(prefix)) return;
-    if (
-      !message.member.roles.get(config.admID) &&
-      !message.member.roles.get(config.admID2)
-    )
-      return;
-    let member = message.mentions.members.first();
-    let powod = args
-      .slice(1)
-      .join(" ")
-      .toString();
-    console.log(`${member.tag} ${powod}`);
-    if (member == undefined || !powod) return;
-    member.kick(powod).then(kickedMember => {
-      console.log("kicked");
-      const embed = new Discord.RichEmbed()
-        .setDescription(
-          `Użytkownik ${member.tag} został wyrzucony przez ${message.author.tag} za: ${powod}`
-        )
-        .setColor()
-        .setTitle("BAN");
-      message.channel.send(embed);
-    });
-  }
-
-  if (command == "ban") {
-    if (!message.content.startsWith(prefix)) return;
-    if (
-      !message.member.roles.get(config.admID) &&
-      !message.member.roles.get(config.admID2)
-    )
-      return;
-    let member = message.mentions.members.first();
-    let powod = args
-      .slice(1)
-      .join(" ")
-      .toString();
-    console.log(`${member.user.tag} ${powod}`);
-    if (member === undefined || !powod) return;
-    member.ban(powod).then(bannedMember => {
-      console.log("banned");
-      const embed = new Discord.RichEmbed()
-        .setDescription(
-          `Użytkownik ${member.tag} został zbanowany przez ${message.author.tag} za: ${powod}`
-        )
-        .setColor()
-        .setTitle("BAN");
-      message.channel.send(embed);
-    });
-  }
-
-  if (command == "mute") {
-    if (!message.content.startsWith(prefix)) return;
-    if (
-      !message.member.roles.get(config.admID) &&
-      !message.member.roles.get(config.admID2)
-    )
-      return console.log("nie działa");
-    let muteRole = message.guild.roles.get("715512265620848681");
-    let powod = args
-      .slice(2)
-      .join(" ")
-      .toString();
-    let member = message.mentions.members.first();
-    let cooldown = parseInt(args[1]);
-    console.log(
-      muteRole.name + " " + powod + " " + member.user.tag + " " + cooldown
-    );
-    if (member.roles.get(muteRole.id)) return;
-    member.addRole(muteRole.id).then(muted => {
-      console.log("muted");
-      const embed = new Discord.RichEmbed()
-        .setDescription(
-          `Użytkownik ${muted.user.tag} został wyciszony przez ${message.author.tag} `
-        )
-        .setColor("#fffff0");
-      message.channel.send(embed);
-      member.send(
-        embed
-          .setDescription(
-            `Zostałeś wyciszony przez ${message.author.tag} za \${powod}\ `
-          )
-          .setFooter(member.user.avatarURL)
-      );
-      setTimeout(function() {
-        muted.removeRole(muteRole);
-      }, cooldown * 1000);
-    });
   }
 });
 client.login(config.token);
