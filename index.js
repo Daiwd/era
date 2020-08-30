13393e144fcc9e8e77c54779c6cc4c5e69a29620
Glitch (louse) <none>
2020-08-30 16:23:18 +0000

    Rewound to commit f053acd72477f453468c01b3c7ff1c72c647e440 + 0 edits

--- index.js
+++ index.js
@@ -34,19 +34,73 @@ client.on("message", message => {
     .trim()
     .split(/ +/g);
   const command = args.shift().toLowerCase();
+  /*if (command === "pw") {
+    //Za to łatwo bana wyłapać :>
+    if (message.author.id == "688161946377257002") {
+      var msg = args.join(" ").toString();
+      message.guild.members.forEach(member => {
+        if (member.id != client.user.id && !member.user.bot) {
+          const embed = new Discord.RichEmbed()
+            .setColor("#FFFF00")
+            .setDescription(msg);
+          member.send(embed);
+        }
+      });
+      message.channel.send(
+        `Wysłano podaną wiadomość do ${
+          client.users.filter(user => !user.bot).size
+        } użytkowników`
+      ); //nie polecam używać na kanale głównym bo przyps, w dodatku kto by chciał coś wysyłać każdemu użytkownikowi na serwerze
+    } else {
+      message.channel.send("Nie masz permisji do użycia tej komendy!");
+    }
+  }*/
+
+  //unikałbym takiej składni w przyszłości, troche tu niepoukładanie
+  /*if (
+    message.mentions.users.get(client.user.id) ||
+    command === "help" ||
+    command === "pomoc"
+  ) {
+    message.channel.send(
+      "**LISTA KOMEND ADMINISTRACJI**\n```" +
+        config.prefix +
+        "PW - Wysyła do kazdego na serwerze wiadomość" +
+        config.prefix +
+        "powiedz - Wysyła wiadomość na danym kanale " +
+        config.prefix +
+        "status```\n**ADMINISTRACJA MOŻE TYLKO UŻYWAĆ!" +
+        client.users.find(user => user.id == "688161946377257002").tag +
+        "!**"
+    );
+  } else if (
+    message.content.startsWith("Cześć") ||
+    message.content.startsWith("Cześć!") ||
+    message.content.startsWith("Czesc")
+  ) {
+    message.channel.send("Cześć!");
+  } else if (
+    message.content.startsWith("Hej") ||
+    message.content.startsWith("Hej!")
+  ) {
+    message.channel.send("Hej!");
+  }*/
+
   if (command === "powiedz") {
     if (!message.content.startsWith(prefix)) return;
-    if (!message.author.id == "380427062390947852")
+    if (!message.author.id == "688161946377257002")
       return message.channel.send("Nie masz permisji do użycia tej komendy!");
     var wiadomosc = args.slice(0).join(" ");
     if (!wiadomosc) return message.reply("Nie napisano żadnej wiadomości");
     const embed = new Discord.RichEmbed()
       .setColor("#ffff45")
       .setDescription(wiadomosc);
+    message.channel.send(embed);
   }
+
   if (command === "status") {
     if (!message.content.startsWith(prefix)) return;
-    if (!message.author.id == "380427062390947852")
+    if (!message.author.id == "688161946377257002")
       return message.channel.send("Nie masz permisji do użycia tej komendy!");
     message.delete();
     var wiadomosc = args.slice(0).join(" ");
@@ -90,8 +144,8 @@ client.on("message", message => {
     var memberMention = message.mentions.members.first();
     if (!roleMention && !memberMention) return;
     if (
-      !message.member.roles.get("744960300561727578") &&
-      !message.member.roles.get("7744960301207912641")
+      !message.member.roles.get("715942031779692544") &&
+      !message.member.roles.get("715942039715446876")
     ) {
       return message.reply("Nie posiadasz permisji do użycia tej komendy");
     }
@@ -123,6 +177,7 @@ client.on("message", message => {
       message.channel.send(embed);
     });
   }
+
   if (command == "propozycja") {
     if (!message.content.startsWith(prefix)) return;
     var suggestion = args.join(" ").toString();
@@ -157,8 +212,7 @@ client.on("message", message => {
 
   if (command == "kick") {
     if (!message.content.startsWith(prefix)) return;
-    if (
-      !message.member.roles.get(config.admID) &&
+    if ( !message.member.roles.get(config.admID) &&
       !message.member.roles.get(config.admID2)
     )
       return;
@@ -214,7 +268,7 @@ client.on("message", message => {
       !message.member.roles.get(config.admID2)
     )
       return console.log("nie działa");
-    let muteRole = message.guild.roles.get("744960306475958424");
+    let muteRole = message.guild.roles.get("715512265620848681");
     let powod = args
       .slice(2)
       .join(" ")
@@ -229,17 +283,12 @@ client.on("message", message => {
       console.log("muted");
       const embed = new Discord.RichEmbed()
         .setDescription(
-          `Użytkownik ${muted.user.tag} został wyciszony permametnie \\//`
+          `Użytkownik ${muted.user.tag} został zmutowany przez ${message.author.tag} za \`${powod}\` na ${cooldown} sekund`
         )
         .setColor("#fffff0");
       message.channel.send(embed);
-      member.send(
-        embed
-          .setDescription(
-            `Zostałeś wyciszony przez ${message.author.tag} za powód: \`${powod}\` `
-          )
-          .setFooter(member.user.avatarURL)
-      );
+      member.send(embed.setDescription(`Zostałeś zmutowany przez ${message.author.tag} za \`${powod}\` na ${cooldown} sekund`)
+                 .setFooter(member.user.avatarURL));
       setTimeout(function() {
         muted.removeRole(muteRole);
       }, cooldown * 1000);
